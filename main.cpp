74303752a16345241cab669d37c96db4ebef9db7
jhytai <josue.tai@csu.fullerton.edu>
2023-11-08 13:44:12 -0800

    Implemented exhaustive search function

--- main.cpp
+++ main.cpp
@@ -9,6 +9,10 @@ using namespace std;
 using namespace std::chrono;
 
 // Function prototypes
+int     totalValue(const vector<pair<int, int>>& items,
+                   const vector<bool>& selection);
+int     totalStocks(const vector<pair<int, int>>& items,
+                    const vector<bool>& selection);
 int     exhaustiveSearch(int M, vector<pair<int, int>> &items);
 int     dynamicProgramming(int M, vector<pair<int, int>> &items);
 void    printTimeTaken(high_resolution_clock::time_point start);
@@ -18,7 +22,7 @@ void    printTimeTaken(high_resolution_clock::time_point start);
 //
 // ============================================================================
 
-int main()
+int     main()
 {
 
 
@@ -29,9 +33,85 @@ int main()
 
 
 
-// ==== exhaustiveSearch ======================================================
+// ==== totalValue ============================================================
+//
+// Helper function to calculate the total value of the stocks
 //
 // ============================================================================
+
+int     totalValue(const vector<pair<int, int>>& items,
+                   const vector<bool>& selection)
+{
+    int total = 0;
+
+    for (size_t i = 0; i < selection.size(); ++i)
+        {
+        if (selection[i])
+            {
+            total += items[i].second;
+            }
+        }
+
+    return total;
+
+}  // end of "totalValue"
+
+
+
+// ==== totalStocks ===========================================================
+//
+// Helper function to calculate the total stocks
+//
+// ============================================================================
+
+int     totalStocks(const vector<pair<int, int>>& items,
+                    const vector<bool>& selection)
+{
+    int stocks = 0;
+
+    for (size_t i = 0; i < selection.size(); ++i)
+        {
+        if (selection[i])
+            {
+            stocks += items[i].first;
+            }
+        }
+
+    return stocks;
+
+}  // end of "totalStocks"
+
+
+
+// ==== exhaustiveSearch ======================================================
+//
+// Exhaustive search implementation
+//
+// ============================================================================
+
+int     exhaustiveSearchUtil(int M, vector<pair<int, int>>& items,
+                             vector<bool>& selection, int index)
+{
+    if (index == items.size())
+        {
+        if (totalValue(items, selection) <= M)
+            {
+            return totalStocks(items, selection);
+            }
+        return 0;
+        }
+
+    // Select the item
+    selection[index] = true;
+    int withItem = exhaustiveSearchUtil(M, items, selection, index + 1);
+
+    // Do not select the item
+    selection[index] = false;
+    int withoutItem = exhaustiveSearchUtil(M, items, selection, index + 1);
+
+    return max(withItem, withoutItem);
+
+}  // end of "exhaustiveSearch"
 
 
 
